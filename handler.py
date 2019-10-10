1e890887a62887f5320aee06e28d0f083ba6bcaa
Sruti Modekurty <msrutiv@gmail.com>
2019-10-10 00:11:31 -0400

    Add serverless framework. Create scraper lambda function

---
+++ handler.py
@@ -0,0 +1,13 @@
+import json
+import scraper
+
+
+def scrape_megabus(event, context):
+    route_info = event
+    date = route_info["date"]
+    origin = route_info["origin"]
+    destination = route_info["destination"]
+
+    data = scraper.get_ticket_price_by_capacity(date, origin, destination)
+
+    return data
--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import json
 import requests
 from bs4 import BeautifulSoup
 import time
+from http import HTTPStatus
 
 
 def pull_data(dep_date, num_tix, start_rt, end_rt):
@@ -9,20 +10,22 @@ def pull_data(dep_date, num_tix, start_rt, end_rt):
 
     params = {
         "departureDate": dep_date,  # format 2019-10-10
-        "originId": start_rt,  # 56 Edinburgh
-        "destinationId": end_rt,  # 34 London
+        "originId": start_rt,
+        "destinationId": end_rt,
         "totalPassengers": num_tix
     }
     resp = requests.get(megabus_url, params)
-    return parse_data(resp.text)
+    if resp.status_code != HTTPStatus.OK:
+        return None
+    return resp.text
 
 
 def parse_data(text):
     soup = BeautifulSoup(text, "html.parser")
     journey_text = soup.find_all('script')[-1].text
     journey_data = journey_text.strip()[len("window.SEARCH_RESULTS = "): -1]
-    journeys = json.loads(journey_data)['journeys']
-    if len(journeys) == 0:
+    journeys = json.loads(journey_data).get('journeys')
+    if journeys == None or len(journeys) == 0:
         return None
     prices = []
     for journey in journeys:
@@ -31,15 +34,16 @@ def parse_data(text):
 
 
 def get_ticket_price_by_capacity(dep_date, start_rt, end_rt):
-    # print("Date: {} Route: {}-{}".format(dep_date, start_rt, end_rt))
+    # print("Date: {} Route: {}-{}".format(dep_date, start_rt, end_rt)) #DEBUG
     all_prices = []
-    for i in range(1, 75):
-        prices = pull_data(dep_date, i, start_rt, end_rt)
-        if prices != None:
-            prices = [total/i for total in prices]
-            all_prices.append(prices)
-            # print("{}:{}".format(i, prices))
-        else:
-            print("{}: None".format(i))
-        time.sleep(.5)  # to prevent getting blocked
+    for i in range(1, 75):  # assume max bus capacity is 74
+        data_text = pull_data(dep_date, i, start_rt, end_rt)
+        if data_text != None:
+            prices = parse_data(data_text)
+            if prices != None:
+                prices = [total/i for total in prices]
+                all_prices.append(prices)
+                # print("{}:{}".format(i, prices)) #DEBUG
+            # else:
+                # print("{}: None".format(i)) #DEBUG
     return all_prices

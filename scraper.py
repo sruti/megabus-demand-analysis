e396757aee16a3706ec11724e8bfa2cb8b0d16a2
Sruti Modekurty <msrutiv@gmail.com>
2019-10-09 22:09:40 -0400

    Add function to scrape for multiple tickets

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import json
 import requests
 from bs4 import BeautifulSoup
+import time
 
 
 def pull_data(dep_date, num_tix, start_rt, end_rt):
@@ -13,7 +14,7 @@ def pull_data(dep_date, num_tix, start_rt, end_rt):
         "totalPassengers": num_tix
     }
     resp = requests.get(megabus_url, params)
-    parse_data(resp.text)
+    return parse_data(resp.text)
 
 
 def parse_data(text):
@@ -21,8 +22,24 @@ def parse_data(text):
     journey_text = soup.find_all('script')[-1].text
     journey_data = journey_text.strip()[len("window.SEARCH_RESULTS = "): -1]
     journeys = json.loads(journey_data)['journeys']
+    if len(journeys) == 0:
+        return None
+    prices = []
     for journey in journeys:
-        print(journey['price'])
+        prices.append(journey['price'])
+    return prices
 
 
-pull_data("2019-10-13", 10, 56, 34)
+def get_ticket_price_by_capacity(dep_date, start_rt, end_rt):
+    # print("Date: {} Route: {}-{}".format(dep_date, start_rt, end_rt))
+    all_prices = []
+    for i in range(1, 75):
+        prices = pull_data(dep_date, i, start_rt, end_rt)
+        if prices != None:
+            prices = [total/i for total in prices]
+            all_prices.append(prices)
+            # print("{}:{}".format(i, prices))
+        else:
+            print("{}: None".format(i))
+        time.sleep(.5)  # to prevent getting blocked
+    return all_prices

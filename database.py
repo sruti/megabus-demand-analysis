0fd30073b248ff7d08b0e8462d9cba87bb25bf76
Sruti Modekurty <msrutiv@gmail.com>
2019-10-15 14:15:35 -0700

    Modify scrape lambda function to put price data in database

--- database.py
+++ database.py
@@ -1,38 +1,25 @@
 import boto3
 
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('servicesTable')
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('journeysTable')
 
 
-def get_data(service_id):
-    response = table.get_item(Key={'service_id': service_id})
-    data = response.get("Item")
-    return data
+def get_journey(journey_id):
+    response = table.get_item(Key={'journey_id': journey_id})
+    journey = response.get("Item")
+    return journey
 
 
-def put_data(data):
-    response = table.put_item(Item=data)
+def put_journey(journey):
+    response = table.put_item(Item=journey)
     return response
 
 
-def update_data(service_id, price_by_capacity):
-    response = table.put_item(Key={'service_id': service_id},
-                              UpdateExpression='SET price_by_capacity = :pbc',
-                              ExpressionAttributeValues={
-        ':pbc': price_by_capacity
+def update_journey(journey_id, price_by_numtix):
+    response = table.update_item(Key={'journey_id': journey_id},
+                                 UpdateExpression='SET price_by_numtix = :pbc',
+                                 ExpressionAttributeValues={
+        ':pbc': price_by_numtix
     })
     return response
-
-
-def set_service_id(origin, destination, start_time, end_time):
-    return origin + destination + start_time[0:2] + start_time[3:5] + end_time[0:2] + end_time[3:5]
-
-
-def extract_service_id(service_id):
-    return {
-        "origin": service_id[0:2],
-        "destination": service_id[2:4],
-        "start_time": service_id[4:6] + ':' + service_id[6:8],
-        "end_time": service_id[8:10] + ':' + service_id[10:12],
-    }
--- handler.py
+++ handler.py
@@ -1,5 +1,8 @@
 import json
 import scraper
+import database
+from datetime import datetime
+from decimal import Decimal
 
 
 def scrape_megabus(event, context):
@@ -8,6 +11,17 @@ def scrape_megabus(event, context):
     origin = route_info["origin"]
     destination = route_info["destination"]
 
-    data = scraper.get_prices(date, origin, destination)
+    journeys_prices = scraper.get_prices(date, origin, destination)
 
-    return data
+    # boto3 float to decimal hack
+    journeys_prices_dump = json.dumps(journeys_prices)
+    journeys_prices = json.loads(journeys_prices_dump, parse_float=Decimal)
+
+    # For now, assuming, journey aready exists in table
+    for journey_id, prices in journeys_prices.items():
+        price_by_numtix = database.get_journey(
+            journey_id).get("price_by_numtix")
+        price_by_numtix[datetime.utcnow().isoformat()] = prices
+        database.update_journey(journey_id, price_by_numtix)
+
+    return journeys_prices
